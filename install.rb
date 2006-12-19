7210312b0dd7110e2235899049c7c3012f307361
Daniel Morrison <daniel@collectiveidea.com>
2006-12-19 14:38:38 +0000

    Copy mock to project mock directory

--- install.rb
+++ install.rb
@@ -1,1 +1,2 @@
-# Install hook code here
+# Copy the mock imap.rb to the project's test directory
+File.copy(File.join(File.dirname(__FILE__), 'test/mocks/imap.rb'), File.join(File.dirname(__FILE__), '../../../test/mocks/test/'))
