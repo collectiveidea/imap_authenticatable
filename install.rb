53e772a90ab05b76b5d78a3f7551192dca2bb92c
Daniel Morrison <daniel@collectiveidea.com>
2006-12-19 14:56:00 +0000

    Fixed copy on installer

--- install.rb
+++ install.rb
@@ -1,2 +1,3 @@
 # Copy the mock imap.rb to the project's test directory
-File.copy(File.join(File.dirname(__FILE__), 'test/mocks/imap.rb'), File.join(File.dirname(__FILE__), '../../../test/mocks/test/'))
+require 'fileutils'
+FileUtils.cp(File.join(File.dirname(__FILE__), 'test/mocks/imap.rb'), File.join(File.dirname(__FILE__), '../../../test/mocks/test/'))
